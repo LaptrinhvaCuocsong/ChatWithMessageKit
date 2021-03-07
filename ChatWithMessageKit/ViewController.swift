f8b463578d85fda15f7a1cfdf51976c5257276e6
HungNM <hung.nguyen@tomosia.com>
2021-03-07 13:27:55 +0700

    Initial Commit

---
+++ ChatWithMessageKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatWithMessageKit
+//
+//  Created by Apple on 07/03/2021.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
